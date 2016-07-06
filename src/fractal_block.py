4a63f03799a7108ddcf2b979bba14ab18eebc910
Akshay Mishra <lck.akshay@gmail.com>
2016-07-06 01:41:35 -0400

    Refactored

--- src/fractal_block.py
+++ src/fractal_block.py
@@ -13,18 +13,21 @@ def join(t):
         joined=tflearn.dropout(tf.reduce_mean(t,0), .5, name="Join")
         return joined
 
-def show(t):
-    print(tensor_shape(t))
-
-def fractal_block(incoming, filters, ncols=4, fsize=[3,3], joined=False):
-    name = "Conv{}".format(ncols)
-    left = [tflearn.conv_2d(incoming, filters, fsize, name=name, activation='relu')]
+    
+def fractal_block(incoming, filters, ncols=4, fsize=[3,3],
+                  joined=False, reuse=False, scope=None, name="FractalBlock"):
+    col = "Col{}".format(ncols)
+    with tf.variable_op_scope([incoming], scope, name, reuse=reuse) as scope:
+        net = fractal_expand(incoming, filters, ncols, fsize, joined)
+    return net
+        
+def fractal_expand(incoming, filters, ncols, fsize=[3,3], joined=False):
+    col = "Col{}".format(ncols)
+    left = [tflearn.conv_2d(incoming, filters, fsize, name=col, activation='relu')]
     if(ncols==1):
         return left
-    right_1 = join(fractal_block(incoming, filters, ncols-1, fsize))
-    right_2 = fractal_block(right_1, filters, ncols-1, fsize)
-    out =  left + right_2
-    if joined:
-        return join(out)
-    else:
-        return out
+    right = join(fractal_expand(incoming, filters, ncols-1, fsize))
+    right = fractal_expand(right, filters, ncols-1, fsize)
+    out =  left + right
+    return join(out) if joined else out
+
