9a547eeff3639baae987052bc85b3284050db20d
Akshay Mishra <lck.akshay@gmail.com>
2016-07-06 00:06:45 -0400

    Removed redundant merges

--- src/fractal_block.py
+++ src/fractal_block.py
@@ -7,15 +7,24 @@ def tensor_shape(t):
     return t.get_shape().as_list()
 
 def join(t):
+    if len(t)==1:
+        return t[0]
     with tf.name_scope("Join"):
         joined=tflearn.dropout(tf.reduce_mean(t,0), .5, name="Join")
         return joined
 
-def fractal_block(incoming, filters, ncols=4, fsize=[3,3]):
-    scope = "Conv{}".format(ncols)
-    left = tflearn.conv_2d(incoming, filters, fsize, name=scope)
+def show(t):
+    print(tensor_shape(t))
+
+def fractal_block(incoming, filters, ncols=4, fsize=[3,3], joined=False):
+    name = "Conv{}".format(ncols)
+    left = [tflearn.conv_2d(incoming, filters, fsize, name=name, activation='relu')]
     if(ncols==1):
         return left
-    right_1 = fractal_block(incoming, filters, ncols-1, fsize)
+    right_1 = join(fractal_block(incoming, filters, ncols-1, fsize))
     right_2 = fractal_block(right_1, filters, ncols-1, fsize)
-    return join([left, right_2])
+    out =  left + right_2
+    if joined:
+        return join(out)
+    else:
+        return out
