72389f6afc83b87b1759539fb7328af4bbd63b9b
Akshay Mishra <lck.akshay@gmail.com>
2016-07-07 12:44:04 -0400

    Implemented global drop path

--- src/fractal_block.py
+++ src/fractal_block.py
@@ -16,11 +16,17 @@ def join(t):
     
 def fractal_block(incoming, filters, ncols=4, fsize=[3,3],
                   joined=True, reuse=False, scope=None, name="FractalBlock"):
-    
+
+    Ws = [[] for _ in range(ncols)]
+    bs = [[] for _ in range(ncols)]
+
     def conv_block(incoming, col):
-        net = tflearn.conv_2d(incoming, filters, fsize,
+        conv = tflearn.conv_2d(incoming, filters, fsize,
                               name='Col{}'.format(col), activation='relu')
-        return net
+        Ws[col].append(conv.W)
+        bs[col].append(conv.b)
+        return conv
+
     def fractal_expand(incoming, col=0):
         left = [conv_block(incoming, col)]
         if(col==ncols-1):
@@ -30,6 +36,30 @@ def fractal_block(incoming, filters, ncols=4, fsize=[3,3],
         out =  left + right
         return out
 
+    
+    def together(name="Fractal"):
+        # with tf.variable_op_scope([incoming], scope, name, reuse=reuse) as scope:
+        with tf.name_scope(name) as scope:
+            out = fractal_expand(incoming)
+            net=join(out) if joined else out
+        return net
+
+    
+    def seperated(name="Columns"):
+        # with tf.variable_op_scope([incoming], scope, name, reuse=reuse) as scope:
+        with tf.name_scope(name) as scope:
+            sep = [incoming] * ncols
+            for col in range(ncols):
+                for W, b in zip(Ws[col], bs[col]):
+                    sep[col] = tf.nn.relu(tf.nn.conv2d(sep[col], W, [1,1,1,1], 'SAME') + b)
+            sep = join(sep)
+        return sep
+
     with tf.variable_op_scope([incoming], scope, name, reuse=reuse) as scope:
-        out = fractal_expand(incoming)
-        return join(out) if joined else out
+        fractal = join(fractal_expand(incoming))
+        columns = seperated()
+        is_training = tflearn.get_training_mode()
+        net = tf.cond(is_training, lambda: columns, lambda: fractal)
+    return net
+
+# remember tf.mul
