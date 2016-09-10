589947d0b43dbcf78d479e54293324a6702081c8
Akshay Mishra <lck.akshay@gmail.com>
2016-09-10 14:06:00 -0500

    Major refactoring

--- src/fractal_block.py
+++ src/fractal_block.py
@@ -1,87 +1,90 @@
-from __future__ import print_function, division
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import functools
+
+from tensorflow.contrib.framework.python.ops import add_arg_scope
+from tensorflow.contrib.framework.python.ops import variables
+from tensorflow.contrib.layers.python.layers import initializers
+from tensorflow.contrib.layers.python.layers import utils
+
+
+from tensorflow.python.framework import dtypes
+from tensorflow.python.framework import ops
+from tensorflow.python.ops import array_ops
+from tensorflow.python.ops import init_ops
+from tensorflow.python.ops import math_ops
+from tensorflow.python.ops import nn
+from tensorflow.python.ops import standard_ops
+from tensorflow.python.ops import variable_scope
+
+
+from tflearn.datasets import mnist
+xflat, _,_,_ = mnist.load_data()
+batch=xflat[:16].reshape(-1,28,28,1)
+net  = batch
+
 import tensorflow as tf
-import numpy as np
-import tflearn
+from tensorflow.contrib import slim
 from tensorflow import transpose as T
 from tensorflow import mul
 
+import tflearn
+
 def tensor_shape(tensor):
-    return t.get_shape().as_list()
+  return tensor.get_shape().as_list()
 
 def local_drop(cols, drop_prob=.85):
-    size = len(cols)-1
-    with tf.variable_op_scope(cols, None, "LocalDropPath"):
-        out = tf.to_float(cols)
-        drop_mask = tf.to_float(tf.concat(0,[[1],tf.random_uniform([size])])>drop_prob)
-        masked = T(mul(T(out),tf.random_shuffle(drop_mask)))
-        dropped = tf.reduce_sum(masked,0)/tf.reduce_sum(drop_mask,0)
-    return dropped
+  size = len(cols)-1
+  with tf.variable_op_scope(cols, None, "LocalDropPath"):
+    out = tf.to_float(cols)
+    drop_mask = tf.to_float(tf.concat(0,[[1],tf.random_uniform([size])])>drop_prob)
+    masked = T(mul(T(out),tf.random_shuffle(drop_mask)))
+    dropped = tf.reduce_sum(masked,0)/tf.reduce_sum(drop_mask,0)
+  return dropped
 
 def join(cols, drop_prob = .15):
-    if len(cols)==1:
-        return cols[0]
-    with tf.variable_op_scope(cols, None, "Join"):
-        joined=tf.reduce_mean(cols,0)
-        out = tf.cond(tflearn.get_training_mode(),
-                      lambda: local_drop(cols, drop_prob), lambda: joined)
-    return joined
+  if len(cols)==1:
+    return cols[0]
+  with tf.variable_op_scope(cols, None, "Join"):
+    joined=tf.reduce_mean(cols,0)
+    out = tf.cond(tflearn.get_training_mode(),
+                  lambda: local_drop(cols, drop_prob), lambda: joined)
+  return joined
 
-def fractal_block(incoming, filters, ncols=3, fsize=[3,3],
-                  joined=True, reuse=False, scope=None, name="FractalBlock"):
-
-    Ws = [[] for _ in range(ncols)]
-    bs = [[] for _ in range(ncols)]
-
-    def conv_block(incoming, col):
-        with tf.variable_op_scope([incoming], None, "Column_{}".format(col)):
-            conv = tflearn.conv_2d(incoming, filters, fsize, weights_init ='xavier',
-                                   activation='linear')
-            net = tflearn.batch_normalization(conv)
-            net = tflearn.activation(net, 'relu')
-        Ws[col].append(conv.W)
-        bs[col].append(conv.b)
-        return net
-
-    def fractal_expand(incoming, col=0):
-        left = [conv_block(incoming, col)]
-        if(col==ncols-1):
-            return left
-        right = join(fractal_expand(incoming, col+1))
-        right = fractal_expand(right, col+1)
-        out =  left + right
-        return out
-
+def fractal_block(inputs,
+                  block,
+                  num_cols,
+                  joined=True,
+                  reuse=False,
+                  variables_collections=None,
+                  outputs_collections=None,
+                  trainable=True,
+                  scope=None):
+  
+  def fractal_expand(inputs, block, num_cols, joined):
+    '''Recursive Helper Function for making fractal'''
+    print(num_cols)
     
-    def together(name="Fractal"):
-        with tf.variable_op_scope([incoming], None, name) as scope:
-            out = fractal_expand(incoming)
-            net=join(out) if joined else out
-        return net
-
-
-    def random_col(cols, name='RandomColumn'):
-        with tf.variable_op_scope(cols, name):
-            col_idx = tf.random_uniform([],0,len(cols),'int32')
-            return tf.gather(cols, col_idx)
+    if num_cols == 1:
+      out = block(inputs)
+      return out if joined else [out]
     
-    def seperated(name="Seperated"):
-        with tf.variable_op_scope([incoming], name) as scope:
-            sep = [incoming] * ncols
-            for col in range(ncols):
-                with tf.variable_op_scope([],None,"Column_{}".format(col)):
-                    for idx, (W, b) in enumerate(zip(Ws[col], bs[col])):
-                        with tf.variable_op_scope([incoming], None, "ConvBlock") as scope:
-                            conv = (tf.nn.conv2d(sep[col], W, [1,1,1,1], 'SAME') + b)
-                            conv = tflearn.batch_normalization(conv)
-                            sep[col] = tf.nn.relu(conv)
-            return random_col(sep)
+    left = block(inputs)
+    right = fractal_expand(inputs, block, num_cols-1, joined=True)
+    right = fractal_expand(right, block, num_cols-1, joined=False)
+    cols=[left]
+    cols.extend(right)
+    out = join(cols) if joined else cols
+    print(out)
+    return out
 
-    is_training = tflearn.get_training_mode()
-    
-    with tf.variable_op_scope([incoming], scope, name, reuse=reuse) as scope:
-        fractal = together()
-        columns = seperated()
-        with tf.variable_op_scope([incoming],"DropPath"):
-            global_drop = tf.logical_and(is_training, tf.random_uniform([])>.5)
-            net = tf.cond(global_drop, lambda: columns, lambda: fractal, name="DropPath")
-    return net
+  with tf.variable_op_scope([inputs], scope, 'Fractal',
+                            reuse=reuse) as scope:
+    net=fractal_expand(inputs, block, num_cols, joined)
+  return net
+
+
+def wrap_fn(fn,*args,**kwargs):
+  return lambda inputs: fn(inputs,*args,**kwargs)
