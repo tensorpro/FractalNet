5c0c18c4ae184f1b16778843cf259a2d3609e193
Akshay Mishra <lck.akshay@gmail.com>
2017-01-24 11:35:01 -0600

    increased num epochs

--- src/cifar_demo.py
+++ src/cifar_demo.py
@@ -65,5 +65,5 @@ net = regression(net, optimizer='adam',
 
 # Train using classifier
 model = tflearn.DNN(net, tensorboard_verbose=0)
-model.fit(X, Y, n_epoch=50, shuffle=True, validation_set=(X_test, Y_test),
+model.fit(X, Y, n_epoch=400, shuffle=True, validation_set=(X_test, Y_test),
           show_metric=True, batch_size=32, run_id='cifar10_cnn')
